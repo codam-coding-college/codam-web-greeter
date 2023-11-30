c3d1736d635691f06d96154cc039d9c033a862ff
Freek Bes <freek.bes@codam.nl>
2023-11-30 17:01:20 +0100

    fix bugs in wallpaper path parsing

--- systemd/user/init-codam-web-greeter.sh
+++ systemd/user/init-codam-web-greeter.sh
@@ -11,7 +11,7 @@ TMP_WALLPAPER_PATH="/tmp/codam-web-greeter-user-wallpaper"
 # Get the picture to be used as a screensaver specified by Gnome.
 # Do not use the desktop wallpaper since there are two: one for light and one for dark mode.
 # The screensaver is always the same and points to the current wallpaper.
-WALLPAPER=$(/usr/bin/gsettings get org.gnome.desktop.screensaver picture-uri | /usr/bin/sed 's/^"\(.*\)"$/\1/')
+WALLPAPER=$(/usr/bin/gsettings get org.gnome.desktop.screensaver picture-uri | /usr/bin/sed "s/^['\"]\(.*\)['\"]$/\1/")
 
 if [ -z "$WALLPAPER" ]; then
 	/usr/bin/echo "No Gnome screensaver wallpaper found"
@@ -20,8 +20,8 @@ fi
 
 /usr/bin/echo "Found Gnome screensaver wallpaper $WALLPAPER"
 
-# Check if the wallpaper is a local file
-if /usr/bin/grep -q '^file://' <<<"$WALLPAPER"; then
+# Check if the wallpaper starts with file://
+if [[ "$WALLPAPER" == file://* ]]; then
 	# Get the path to the wallpaper
 	WALLPAPER_PATH=$(/usr/bin/sed 's/^file:\/\///' <<<"$WALLPAPER")
 
@@ -29,6 +29,7 @@ if /usr/bin/grep -q '^file://' <<<"$WALLPAPER"; then
 	if [ -f "$WALLPAPER_PATH" ]; then
 		# Copy the wallpaper to /tmp (without extension)
 		/usr/bin/cp "$WALLPAPER_PATH" "$TMP_WALLPAPER_PATH"
+		/usr/bin/echo "Copied wallpaper $WALLPAPER_PATH to $TMP_WALLPAPER_PATH"
 	else
 		/usr/bin/echo "Wallpaper $WALLPAPER_PATH does not exist"
 	fi
