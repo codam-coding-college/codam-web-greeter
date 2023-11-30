79c12ac0fcc89f4b9f324b651c055f52947a6f38
Freek Bes <freek.bes@codam.nl>
2023-11-30 16:50:31 +0100

    fix path to user service scripts

--- systemd/install.sh
+++ systemd/install.sh
@@ -15,8 +15,8 @@ ROOT_DIR="$(/usr/bin/dirname "$(/usr/bin/readlink -f "$0")")"
 # Install service scripts to /usr/share/codam
 /usr/bin/mkdir -p /usr/share/codam
 /usr/bin/cp "$ROOT_DIR/system/fetch-codam-web-greeter-data.sh" /usr/share/codam/fetch-codam-web-greeter-data.sh
-/usr/bin/cp "$ROOT_DIR/system/init-codam-web-greeter.sh" /usr/share/codam/init-codam-web-greeter.sh
-/usr/bin/cp "$ROOT_DIR/system/cleanup-codam-web-greeter.sh" /usr/share/codam/cleanup-codam-web-greeter.sh
+/usr/bin/cp "$ROOT_DIR/user/init-codam-web-greeter.sh" /usr/share/codam/init-codam-web-greeter.sh
+/usr/bin/cp "$ROOT_DIR/user/cleanup-codam-web-greeter.sh" /usr/share/codam/cleanup-codam-web-greeter.sh
 
 # Copy uninstall script to /usr/share/codam
 /usr/bin/cp "$ROOT_DIR/uninstall.sh" /usr/share/codam/uninstall-codam-web-greeter-service.sh
