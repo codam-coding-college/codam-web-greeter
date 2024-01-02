fed7076ac02460942d23e62ea60edb27c8d5fcf1
Freek Bes <freek.bes@codam.nl>
2024-01-02 17:24:47 +0100

    bugfixes

--- systemd/system/codam-web-greeter-idler.sh
+++ systemd/system/codam-web-greeter-idler.sh
@@ -12,7 +12,6 @@ while IFS= read -r line; do
 	USERNAME=$(echo "$line" | awk '{print $1}')
 	# Get display
 	DISPLAY=$(echo "$line" | awk '{print $5}' | sed 's/[(|)]//g')
-	/usr/bin/echo "Checking idletime for user $USERNAME on display $DISPLAY"
 	# Go to next line if display does not start with :
 	if ! [[ "$DISPLAY" =~ ^: ]]; then
 		continue
@@ -21,7 +20,7 @@ while IFS= read -r line; do
 	IDLE_TIME=$(/usr/bin/sudo -u "$USERNAME" DISPLAY="$DISPLAY" /usr/bin/xprintidle)
 	# Check if session has been idle for over 42 minutes
 	if [ "$IDLE_TIME" -gt 2520000 ]; then
-		/usr/bin/echo "Session for user $USERNAME has been idle for over 42 minutes, forcing logout now"
+		/usr/bin/echo "Session for user $USERNAME has been idle for over 42 minutes (idletime $IDLE_TIME ms), forcing logout now"
 		/usr/bin/systemd-run --wait --user --machine "$USERNAME@.host" /usr/bin/gnome-session-quit --force
 	else
 		/usr/bin/echo "Session for $USERNAME has been idle for $((IDLE_TIME / 1000)) seconds"
