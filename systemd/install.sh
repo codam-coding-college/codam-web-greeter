b054409692e5a78bccb4d8ebb50e9a21fcbb8596
Freek Bes <freek.bes@codam.nl>
2024-01-02 17:02:57 +0100

    fix automatic logout script
    now runs as root to make sure students cannot stop the automated logouts

--- systemd/install.sh
+++ systemd/install.sh
@@ -21,12 +21,14 @@ fi
 # Install service scripts to /usr/share/codam
 /usr/bin/mkdir -p /usr/share/codam
 /usr/bin/cp "$ROOT_DIR/system/codam-web-greeter-fetcher.sh" /usr/share/codam/codam-web-greeter-fetcher.sh
+/usr/bin/cp "$ROOT_DIR/system/codam-web-greeter-idler.sh" /usr/share/codam/codam-web-greeter-idler.sh
+/usr/bin/chmod 700 /usr/share/codam/codam-web-greeter-idler.sh
 /usr/bin/cp "$ROOT_DIR/user/codam-web-greeter-init.sh" /usr/share/codam/codam-web-greeter-init.sh
 /usr/bin/cp "$ROOT_DIR/user/codam-web-greeter-cleanup.sh" /usr/share/codam/codam-web-greeter-cleanup.sh
-/usr/bin/cp "$ROOT_DIR/user/codam-web-greeter-activator.sh" /usr/share/codam/codam-web-greeter-activator.sh
 
 # Copy uninstall script to /usr/share/codam
 /usr/bin/cp "$ROOT_DIR/uninstall.sh" /usr/share/codam/uninstall-codam-web-greeter-service.sh
+/usr/bin/chmod 700 /usr/share/codam/uninstall-codam-web-greeter-service.sh
 
 # Create system user if it doesn't exist
 if id codam-web-greeter >/dev/null 2>&1; then
@@ -44,14 +46,14 @@ DATA_FILE="$WEB_GREETER_DIR/data.json"
 /usr/bin/chown codam-web-greeter:codam-web-greeter "$DATA_FILE"
 /usr/bin/echo '{"error": "No data fetched yet"}' > "$DATA_FILE"
 
-# Install systemd system service and timer
+# Install systemd system services and timers
 /usr/bin/cp "$ROOT_DIR/system/codam-web-greeter.service" /etc/systemd/system/codam-web-greeter.service
 /usr/bin/cp "$ROOT_DIR/system/codam-web-greeter.timer" /etc/systemd/system/codam-web-greeter.timer
+/usr/bin/cp "$ROOT_DIR/system/codam-web-greeter-idler.service" /etc/systemd/system/codam-web-greeter-idler.service
+/usr/bin/cp "$ROOT_DIR/system/codam-web-greeter-idler.timer" /etc/systemd/system/codam-web-greeter-idler.timer
 
-# Install systemd user services
+# Install systemd user service
 /usr/bin/cp "$ROOT_DIR/user/codam-web-greeter.service" /etc/systemd/user/codam-web-greeter.service
-/usr/bin/cp "$ROOT_DIR/user/codam-web-greeter-activator.service" /etc/systemd/user/codam-web-greeter-activator.service
-/usr/bin/cp "$ROOT_DIR/user/codam-web-greeter-activator.timer" /etc/systemd/user/codam-web-greeter-activator.timer
 
 # Reload systemd daemon
 /usr/bin/systemctl daemon-reload
@@ -60,10 +62,11 @@ DATA_FILE="$WEB_GREETER_DIR/data.json"
 # Enable and start systemd system timer
 /usr/bin/systemctl enable codam-web-greeter.timer
 /usr/bin/systemctl start codam-web-greeter.timer
+/usr/bin/systemctl enable codam-web-greeter-idler.timer
+/usr/bin/systemctl start codam-web-greeter-idler.timer
 
 # Fetch data for the first time (in the background)
 /usr/bin/systemctl start codam-web-greeter.service &
 
-# Enable and start systemd user service for all users
+# Enable systemd user service for all users
 /usr/bin/systemctl --global enable codam-web-greeter.service
-/usr/bin/systemctl --global enable codam-web-greeter-activator.timer
