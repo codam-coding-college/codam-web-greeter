da8c548af03daa8d042d10dcafaa3417428e56bb
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2024-05-15 12:05:32 +0200

    Add support for multiple exams at the same time (#15)

--- client/uis/screens/examscreen.ts
+++ client/uis/screens/examscreen.ts
@@ -8,7 +8,8 @@ export class ExamModeUI extends UIScreen {
 	public static readonly EXAM_PASSWORD: string = 'exam';
 
 	public readonly _form: UIExamModeElements;
-	private _exam: ExamForHost | null = null;
+	private _examMode: boolean = false;
+	private _examIds: number[] = [];
 	private _loginScreen: LoginScreenUI;
 	protected _events: AuthenticatorEvents = {
 		authenticationStart: () => {
@@ -30,7 +31,7 @@ export class ExamModeUI extends UIScreen {
 		},
 	};
 
-	public constructor(auth: Authenticator, loginUI: LoginScreenUI, exam: ExamForHost | null = null) {
+	public constructor(auth: Authenticator, loginUI: LoginScreenUI) {
 		super(auth);
 
 		// Keep a reference to the login screen so that we can show it when the exam is over
@@ -45,35 +46,46 @@ export class ExamModeUI extends UIScreen {
 		} as UIExamModeElements;
 
 		this._initForm();
-
-		if (exam !== null) {
-			this.setExam(exam);
-		}
 	}
 
 	/**
-	 * Set the exam to display on the exam mode screen.
-	 * If no exam is given, the exam mode screen will be hidden and the login screen will be shown instead.
-	 * @returns true if the exam mode screen should be shown, false if the login screen is shown instead
+	 * Enable exam mode for a list of exams currently ongoing (usually just 1).
+	 * If the array of exams is empty, nothing will happen.
 	 */
-	public setExam(exam: ExamForHost | null): boolean {
-		this._exam = exam;
-		this._populateData();
-
-		if (this._exam === null) {
-			this.hideForm();
-			this._loginScreen.showForm();
-			return false;
+	public enableExamMode(exams: ExamForHost[]): void {
+		if (exams.length === 0) {
+			return;
 		}
-		else {
-			this._loginScreen.hideForm();
-			this.showForm();
-			return true;
-		}
+		this._examMode = true;
+		this._examIds = exams.map((exam) => exam.id);
+		this._populateData(exams);
+		this._loginScreen.hideForm();
+		this.showForm();
 	}
 
-	public get exam(): ExamForHost | null {
-		return this._exam;
+	/**
+	 * Disable exam mode and show the default login screen instead.
+	 */
+	public disableExamMode(): void {
+		this._examMode = false;
+		this._examIds = [];
+		this._populateData([]);
+		this.hideForm();
+		this._loginScreen.showForm();
+	}
+
+	/**
+	 * Get whether the exam mode screen is currently displayed.
+	 */
+	public get examMode(): boolean {
+		return this._examMode;
+	}
+
+	/**
+	 * Get the ids of the exams that are currently displayed on the exam mode screen.
+	 */
+	public get examIds(): number[] {
+		return this._examIds;
 	}
 
 	protected _initForm(): void {
@@ -82,40 +94,56 @@ export class ExamModeUI extends UIScreen {
 		// This event gets called when the user clicks the unlock button or submits the lock screen form in any other way
 		form.examStartButton.addEventListener('click', (event: Event) => {
 			event.preventDefault();
-			if (this._exam !== null) {
-				// Always log in with the username and password given by the back-end server.
-				// If no username and password are given, use the default username and password.
+			if (this._examMode) {
 				this._auth.login(ExamModeUI.EXAM_USERNAME, ExamModeUI.EXAM_PASSWORD);
-			}
-			else {
-				console.error('Exam is null');
-				window.ui.setDebugInfo('Exam is null');
 			}
 		});
 	}
 
-	private _populateData(): void {
+	private _populateData(examsToPopulate: ExamForHost[]): void {
 		const form = this._form as UIExamModeElements;
 
-		if (this._exam === null) {
+		if (examsToPopulate.length === 0) {
 			// Unset text that states which exams can be started today
 			form.examProjectsText.innerText = '';
+			form.examStartText.innerText = 'unknown';
+			form.examEndText.innerText = 'unknown';
 		}
 		else {
-			// Populate text that states which exams can be started today
-			const exam = window.data.dataJson?.exams.find((exam) => exam.id === this._exam?.id);
-			if (exam === undefined) {
-				console.error('Exam not found in data.json');
-				window.ui.setDebugInfo('Exam not found in data.json');
+			// Find all exams in the data.json file that match the ids in the exams variable
+			const exams = window.data.dataJson?.exams.filter((exam) => examsToPopulate.some((examToPopulate) => exam.id === examToPopulate.id));
+
+			if (exams === undefined) {
+				console.error('Failed to find exams in data.json');
+				window.ui.setDebugInfo('Failed to find exams in data.json');
 				return;
 			}
-			const projectsText = exam.projects.map((project) => project.name).join(', ');
-			form.examProjectsText.innerText = projectsText;
 
-			const examStart = new Date(this._exam.begin_at);
-			const examEnd = new Date(this._exam.end_at);
-			form.examStartText.innerText = examStart.toLocaleTimeString("en-NL", { hour: '2-digit', minute: '2-digit' });
-			form.examEndText.innerText = examEnd.toLocaleTimeString("en-NL", { hour: '2-digit', minute: '2-digit' });
+			// Find the earliest start time for an exam that should be displayed right now
+			const earliestExam = exams.reduce((earliest, exam) => {
+				const beginAt = new Date(exam.begin_at);
+				if (earliest === null || beginAt < earliest) {
+					return beginAt;
+				}
+				return earliest;
+			}, new Date(exams[0].begin_at));
+
+			// Find the latest end time for an exam that should be displayed right now
+			const latestExam = exams.reduce((latest, exam) => {
+				const endAt = new Date(exam.end_at);
+				if (latest === null || endAt > latest) {
+					return endAt;
+				}
+				return latest;
+			}, new Date(exams[0].end_at));
+
+			// Combine all possible projects for exams that can be started right now
+			const projectsText = exams.flatMap((exam) => exam.projects.map((project) => project.name)).join(', ');
+
+			// Display the projects and the time range in which the exams can be started
+			form.examProjectsText.innerText = projectsText;
+			form.examStartText.innerText = earliestExam.toLocaleTimeString("en-NL", { hour: '2-digit', minute: '2-digit' });
+			form.examEndText.innerText = latestExam.toLocaleTimeString("en-NL", { hour: '2-digit', minute: '2-digit' });
 		}
 	}
 
