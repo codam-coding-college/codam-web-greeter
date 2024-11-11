c4fbeec10d29960f33859190cbd317959dcaf934
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2024-11-11 15:24:42 +0100

    Idle logout rework (#28)
    Rework of the idling system. Instead of assuming the screen was locked right as the greeter was started and using xprintidle to get idletime, get the screen lock time from a file in /tmp

--- systemd/system/codam-web-greeter-idler.sh
+++ systemd/system/codam-web-greeter-idler.sh
@@ -17,13 +17,29 @@ while IFS= read -r line; do
 	if ! [[ "$DISPLAY" =~ ^: ]]; then
 		continue
 	fi
+
 	# Get idle time from X-session using sudo
+	# This time is used to determine if the session has been idle for too long (possibly without locking the screen)
 	IDLE_TIME=$(/usr/bin/sudo -u "$USERNAME" DISPLAY="$DISPLAY" /usr/bin/xprintidle)
-	# Check if session has been idle for over 42 minutes
-	if [ "$IDLE_TIME" -gt 2520000 ]; then
-		/usr/bin/echo "Session for user $USERNAME has been idle for over 42 minutes (idletime $IDLE_TIME ms), forcing logout now by restarting lightdm"
+
+	# Check if lock_timestamp file exists, and if so read the locked_at_timestamp
+	# This time is used to determine if the screen lock has been active for too long
+	# Sometimes xprintidle doesn't work properly when the screen is locked due to programs running in the user session in the background
+	TIME_SINCE_LOCK=$((0)) # Placeholder
+	if [ -f "/tmp/codam_web_greeter_lock_timestamp_$USERNAME" ]; then
+		# Get the locked_at_timestamp from the file
+		LOCKED_AT_TIMESTAMP=$(/usr/bin/awk '{print $1}' "/tmp/codam_web_greeter_lock_timestamp_$USERNAME")
+		# Calculate the time since the session was locked
+		TIME_SINCE_LOCK=$((($(date +%s) - LOCKED_AT_TIMESTAMP) * 1000))
+	fi
+
+	# Check if session has been idle for long enough
+	MAX_IDLE_TIME_MINUTES=$((42))
+	MAX_IDLE_TIME=$((MAX_IDLE_TIME_MINUTES * 60 * 1000))
+	if [ "$IDLE_TIME" -gt "$MAX_IDLE_TIME" ] || [ "$TIME_SINCE_LOCK" -gt "$MAX_IDLE_TIME" ]; then
+		/usr/bin/echo "Session for user $USERNAME has been idle for over 42 minutes (idletime $IDLE_TIME ms, time_since_lock $TIME_SINCE_LOCK ms), forcing logout now by restarting lightdm"
 		/usr/bin/systemctl restart lightdm
 	else
-		/usr/bin/echo "Session for $USERNAME has been idle for $((IDLE_TIME / 1000)) seconds"
+		/usr/bin/echo "Session for $USERNAME has been idle for $((IDLE_TIME / 1000)) seconds, screen locked for $((TIME_SINCE_LOCK / 1000)) seconds"
 	fi
 done <<< "$WHO_OUTPUT"
