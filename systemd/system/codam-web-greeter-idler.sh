aea6b00a37899cf9e35d4ec5232db044dbc2b02f
Freek Bes <freek.bes@codam.nl>
2025-08-20 15:54:05 +0200

    use loginctl to terminate idle user sessions

--- systemd/system/codam-web-greeter-idler.sh
+++ systemd/system/codam-web-greeter-idler.sh
@@ -43,8 +43,8 @@ while IFS= read -r line; do
 	MAX_IDLE_TIME_MINUTES=$((42))
 	MAX_IDLE_TIME=$((MAX_IDLE_TIME_MINUTES * 60 * 1000))
 	if [ "$IDLE_TIME" -gt "$MAX_IDLE_TIME" ] || [ "$TIME_SINCE_LOCK" -gt "$MAX_IDLE_TIME" ]; then
-		/usr/bin/echo "Session for user $USERNAME has been idle for over 42 minutes (idletime $IDLE_TIME ms, time_since_lock $TIME_SINCE_LOCK ms), forcing logout now by restarting lightdm"
-		/usr/bin/systemctl restart lightdm
+		/usr/bin/echo "Session for user $USERNAME has been idle for over 42 minutes (idletime $IDLE_TIME ms, time_since_lock $TIME_SINCE_LOCK ms), forcing logout now"
+		/usr/bin/loginctl terminate-user "$USERNAME" && /usr/bin/systemctl restart lightdm
 	else
 		/usr/bin/echo "Session for $USERNAME has been idle for $((IDLE_TIME / 1000)) seconds, screen locked for $((TIME_SINCE_LOCK / 1000)) seconds"
 	fi
